4f5ee94106978d04dac3e5a199a0050980f02ef2
Romain de Wolff <rdewolff@gmail.com>
2014-05-24 19:36:24 +0200

    New sstructure, config file

--- test/test.js
+++ test/test.js
@@ -1,32 +1,35 @@
 // lib
-var ria = require('./lib/ria.js');
+var ria = require('../lib/ria.js');
 var fs = require('fs');
+var config = require('./config');
 
-// vars
-var KEY_FILE = 'session.key';
-var ENCODING = 'utf8';
+// load config vars
+var CFG_URL = config.get('url'); // example : https://mp-ria-X.zetcom.com/MpWeb-instanceName
+var CFG_USERNAME = config.get('username');
+var CFG_PASSWORD = config.get('password');
+var CFG_KEY_FILE = config.get('keyfile');
+var CFG_ENCODING = config.get('encoding');
 
-// instance de ZIS - works from within Zetcom network
-// ria.setCreditentials('RDW', 'RDW');
-// ria.setInstanceUrl('https://mp-ria-14.zetcom.com/MpWeb-ZetcomZis');
+// setup ria access
+ria.setInstanceUrl(CFG_URL);
+ria.setCreditentials(CFG_USERNAME, CFG_PASSWORD);
 
-// instance de test - works from any internet connection
-ria.setCreditentials('SuperAdmin', 'SuperAdmin');
-ria.setInstanceUrl('https://mp-ria-15.zetcom.com/MpWeb-apParisPuig');
-
-// login if required
-fs.readFile(KEY_FILE, ENCODING, function(err, data){
+// try to read the session file containing the session key
+fs.readFile(CFG_KEY_FILE, CFG_ENCODING, function(err, data){
+	// login if no session file
 	if (err) {
 		console.time('login');
-		ria._login(function(){ 
+		ria._login(function(){
+
 			console.timeEnd('login');
 			// try to reuse session to avoid re-login on every request
 			// on login, save the key to the file
-			fs.writeFile(KEY_FILE, ria.getSessionKey(), ENCODING, null);
+			fs.writeFile(CFG_KEY_FILE, ria.getSessionKey(), CFG_ENCODING, null);
 			console.log("SAVE KEY " + ria.getSessionKey())
 			// ria.deleteSession(runMyTests); // test delete session
 			runMyTests();
 		});
+	// there is session key we can retrieve
 	} else {
 		// retrieved key can be wrong or not valid anymore
 		ria.setSessionKey(data);
@@ -42,18 +45,18 @@ fs.readFile(KEY_FILE, ENCODING, function(err, data){
 function runMyTests() {
 
 	// ************************************************************************
-	// * Standard function tests 
+	// * Standard function tests
 	// ************************************************************************
-	
+
 	//ria.getModuleDefinition('Multimedia', callback);
-	// ria.getModuleDefinition('Object', callback);
-	
+	//ria.getModuleDefinition('Object', callback);
+
 	//ria.getAllModuleDefinition(callback);
-	
+
 	//ria.getModuleItem('Multimedia', '446', callback); // TODO not working
 	//ria.getModuleItem('Multimedia', 'MulAltimaCreationdate0043Txt', callback); // TODO not working
 	//ria.getModuleItem('Multimedia', 84, callback); // TODO not working, ID? How to find them? With the search :)
-	// ria.getModuleItem('Object', '65', callback); 
+	// ria.getModuleItem('Object', '65', callback);
 
 	//ria.getModuleOrgunits('Multimedia', callback); // OK
 
@@ -67,24 +70,26 @@ function runMyTests() {
 	// ria.getVocabularyGroup('425', callback); // OK 50% : returned XML seems INVALID ?
 	// ria.getVocabularyGroup('397', callback);
 	// ria.getVocabularyGroup(373, callback); // OK
-	
+
 	// ria.getVocabulary('3676', callback); // OK
 	// ria.getVocabulary(3139, callback); // OK
 
-	// Body definition : http://www.zetcom.com/ria/ws/module/search/search.xml 
+	// Body definition : http://www.zetcom.com/ria/ws/module/search/search.xml
 	// OK!
-	// ria.postModuleSearch('Multimedia', '<application xmlns="http://www.zetcom.com/ria/ws/module/search" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.zetcom.com/ria/ws/module/search http://www.zetcom.com/ria/ws/module/search/search_1_0.xsd"><modules><module name="Multimedia"><search><fulltext>*</fulltext></search></module></modules></application>', callback); 
+	// ria.postModuleSearch('Multimedia', '<application xmlns="http://www.zetcom.com/ria/ws/module/search" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.zetcom.com/ria/ws/module/search http://www.zetcom.com/ria/ws/module/search/search_1_0.xsd"><modules><module name="Multimedia"><search><fulltext>*</fulltext></search></module></modules></application>', callback);
 
 
 
 	// ************************************************************************
-	// * Custom function tests 
-	// ************************************************************************	
+	// * Custom function tests
+	// ************************************************************************
 	console.time('runMyTests');
-	ria.getModuleList(callback, 'array')
+	ria.getModuleList(callback, 'array');
+
+	// ria.getModuleFields('Object');
 
 	// Complete test
-	//ria.getModuleItem('Object', '65', callback); 
+	//ria.getModuleItem('Object', '65', callback);
 	//ria.getAllObjectFromModule('Object', callback, 'json');
 
 }
@@ -96,4 +101,3 @@ function callback(err, data) {
 	console.log("Data: %j", data); // show all json format
 
 }
-
